1e014c70098d5806756e22cb4d89dc822fb23ee9
Gabriele Fantechi <fantechi76@gmail.com>
2023-05-11 09:39:41 +0200

    final final final final forse

--- utils_3.c
+++ utils_3.c
@@ -6,7 +6,7 @@
 /*   By: gfantech <gfantech@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 14:18:28 by gfantech          #+#    #+#             */
-/*   Updated: 2023/05/10 17:45:49 by gfantech         ###   ########.fr       */
+/*   Updated: 2023/05/11 09:37:59 by gfantech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 int	check_fd(int *fd, char *filename, int i_o, bool append)
 {
-	if (access(filename, F_OK) == 0 && i_o == IN)
+	if (access(filename, F_OK) == -1 && i_o == IN)
+		return (-1);
+	if ((access(filename, R_OK) == 0 && i_o == IN)
+		|| (access(filename, W_OK) == 0 && i_o == OUT)
+		|| (access(filename, F_OK) == -1 && i_o == OUT))
 	{
-		if ((access(filename, R_OK) == 0 && i_o == IN)
-			|| (access(filename, W_OK) == 0 && i_o == OUT))
+		if (*fd != 0)
+			close(*fd);
+		if (i_o == IN)
+			*fd = open(filename, O_RDONLY);
+		else
 		{
-			if (*fd != 0)
-				close(*fd);
-			if (i_o == IN)
-				*fd = open(filename, O_RDONLY);
+			if (append)
+				*fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
 			else
-			{
-				if (append)
-					*fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-				else
-					*fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			}
-			return (0);
+				*fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		}
+		return (0);
 	}
 	return (-1);
 }
